d88e54b734d40daa1dfc13142cdbbd36f496184e
Joe Foster <jfoster29492@gmail.com>
2021-02-08 22:38:20 +0000

    more pets imgs, additional styling
    also added event listeners

--- cyberpet.js
+++ cyberpet.js
@@ -118,6 +118,13 @@ const dogImg = document.querySelector("#dogImg");
 const catImg = document.querySelector("#catImg");
 const rabImg = document.querySelector("#rabImg");
 const petImg = document.querySelector(".petImg");
+const dogImg2 = document.querySelector("#dogImg2")
+const catImg2 = document.querySelector("#catImg2")
+const rabbitImg2 = document.querySelector("#rabImg2")
+const petImg2 = document.querySelector(".petImg2");
+const dogText = document.querySelector("#selectDog");
+const catText = document.querySelector("#selectCat");
+const rabText = document.querySelector("#selectRab");
 
 start.addEventListener("click", (event) => {
     choose.style.display = "block";
@@ -127,6 +134,33 @@ start.addEventListener("click", (event) => {
 choose.addEventListener("click", (event) => {
     petImg.style.display = "block";
     choose.style.display = "none";
+})
+
+dogImg.addEventListener("click", (event) => {
+    petImg.style.display = "none";
+    petImg2.style.display = "block";
+    catImg2.style.display = "none";
+    rabbitImg2.style.display = "none";
+    dogImg2.style.display = "block";
+    selectDog.style.display = "block";
+})
+
+catImg.addEventListener("click", (event) => {
+    petImg.style.display = "none";
+    petImg2.style.display = "block";
+    catImg2.style.display = "block";
+    rabbitImg2.style.display = "none";
+    dogImg2.style.display = "none";
+    selectCat.style.display = "block";
+})
+
+rabImg.addEventListener("click", (event) => {
+    petImg.style.display = "none";
+    petImg2.style.display = "block";
+    catImg2.style.display = "none";
+    rabbitImg2.style.display = "block";
+    dogImg2.style.display = "none";
+    selectRab.style.display = "block";
 })
 
 // let petImg = document.getElementById("petImg").src = "./img/" + diceRoll + ".png";
