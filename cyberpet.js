e04eabb80f256b9459c479d4a7eaf905b94ad204
Joe Foster <jfoster29492@gmail.com>
2021-02-08 19:37:54 +0000

    file creation / initial code

---
+++ cyberpet.js
@@ -0,0 +1,108 @@
+class Animal {
+    constructor(name){
+        this._name = name;
+        this._happiness = 40;
+        this._hunger = 100;
+        this._thirst = 100;
+    }
+    get name(){
+        return this._name;
+    }
+    get play(){
+        return this._play;
+    }
+    get hunger(){
+        return this._hunger;
+    }
+    get thirst(){
+        return this._thirst;
+    }
+    eat(){
+        return this._hunger--;
+    }
+    drink(){
+        return this._thirst--; 
+    }
+    play(){
+        return this._happiness++; 
+    }
+    noplay(){
+        return (`${this.name} is bored!`);
+    }
+    wantDrink(){
+        return (`${this.name} wants a drink!`);
+    }
+    wantFood(){
+        return (`${this.name} wants feeding!`);
+    }
+
+}
+
+
+class Dog extends Animal {
+    constructor(name, walk, bath){
+        super(name);
+        this._walk = walk;
+        this._bath = bath;
+    }
+    get walk(){
+        if (this.happiness == false);
+        return `${this.name} needs a walk!`;
+    }
+    get bath(){
+        if (this.walk == true);
+        return `${this.name} needs a bath!`;
+    }
+
+    
+}
+
+class Cat extends Animal {
+    constructor(name, needsBrush, needsCuddle){
+        super(name);
+        this._needsBrush = needsBrush;
+        this._needsCuddle = needsCuddle;
+    }
+    get needsBrush(){
+        return this._needsBrush;
+    }
+    get needsCuddle(){
+        return this._needsCuddle;
+    }
+}
+
+class Rabbit extends Animal {
+    constructor(name, needsClean, needsRun){
+        super(name);
+        this._needsClean = needsClean;
+        this._needsRun = needsRun;
+    }
+    get needsClean(){
+        return this._needsClean;
+    }
+    get needsRun(){
+        return this._needsRun;
+    }
+}
+
+const poppy = new Dog(
+    "Poppy",
+    true,
+    false,
+);
+
+poppy.happiness = 50;
+poppy.hunger = 20;
+poppy.thirst = 20;
+
+poppy.play();
+poppy.eat();
+poppy.drink();
+poppy.play();
+poppy.play();
+poppy.eat();
+poppy.drink();
+poppy.drink();
+
+console.table(poppy); //stats
+console.log(poppy.noplay());
